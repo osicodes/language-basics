c05b59a8d5310244cd8fe3d10eb848f1046411ca
Osinachi Mbah <osinachi99@gmail.com>
2025-10-26 19:42:32 +0200

    julia  basics

---
+++ julia/basics.jl
@@ -0,0 +1,15 @@
+println("Osinachi")
+
+# for loop
+for i=1:5
+    println(i)
+end
+
+x::Int = 12 #  Define variable 'x' with tyoe 'int'
+
+typeof(x) # get the type of variable 'x'
+
+y::Int = x^2
+
+
+println("y is ",y)
